f5877b84e9058b36f0b088b979118d8bdf080860
Suvid Sahay <42059321+suvidsahay@users.noreply.github.com>
2020-10-04 03:22:44 +0530

    Update binomialcofficient.cpp

--- mathematics/binomialcofficient.cpp
+++ mathematics/binomialcofficient.cpp
@@ -8,89 +8,52 @@
 using namespace std;
 
 ll fact[maxn];
-
 ll inv[maxn];
 
-ll exponentiation(ll n,ll p){
-
-    ll r=1; 
-
-    while(p>0){
-
-      if(p&1){
-          r=(r * n)%mod;
-
-          p--;
-      }
-      else{
-        
-       p=p/2;
-
-       n=(n * n)%mod;
-
-      }
-      
+ll exponentiation(ll n, ll p){
+    ll r = 1; 
+    while(p > 0){
+        if(p & 1){
+            r = (r * n) % mod;
+            p--;
+        }
+        else{
+            p = p / 2;
+            n = (n * n) % mod;
+        } 
     }
-
     return r;
- 
 }
 
-ll cal_nck(ll n,ll k){
-  
-  ll res , r;
-
-   if(n>=k){
-
-   	    r = ( fact[n]* inv[k] )%mod;
-
-            res = (r * inv[n-k])%mod;
-
-            return res;
-
+ll cal_nck(ll n, ll k){
+    ll res, r;
+    if(n >= k){
+   	    r = ( fact[n] * inv[k] ) % mod;
+        res = (r * inv[n - k]) % mod;
+        return res;
       }
-  
-  return 0;
-
+    return 0;
 }
 
 void factorial_andinversefactorial(){
-	
 	fact[0]=1;
-
 	inv[0]=1;
-
-	 for(int i=1;i<maxn;i++){
-          
-             fact[i] = (i * fact[i-1])%mod;
-      
-             inv[i] = exponentiation( fact[i],mod-2 );
-
-           }
-
-
+    for(int i = 1; i < maxn; i++){
+        fact[i] = (i * fact[i - 1]) % mod;
+        inv[i] = exponentiation( fact[i], mod - 2 );
+    }
 }
 
 
 int main(){
-
-  factorial_andinversefactorial();
-
-  int test;
-
-  cin>>test;
-
-  while(test--){
-
-     ll n,k;
-
-     cin>>n>>k;
-
-     cout<<cal_nck(n,k)<<"\n";
-
-     }   
-
-
+    factorial_andinversefactorial();
+    int test;
+    cin >> test;
+    while(test--){
+        ll n, k;
+        cin >> n >> k;
+        cout << cal_nck(n, k) << "\n";
+    }   
 }
 
 
