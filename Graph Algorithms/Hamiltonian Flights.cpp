6228b7908283fa26460c3ea943ff3ed2d942bf2d
Rohankumar2804 <rohan_1901ee48@iitp.ac.in>
2020-10-03 16:47:33 +0530

    Update Hamiltonian Flights.cpp
    i have added spaces and erased unused variable 
    thanks

--- Graph Algorithms/Hamiltonian Flights.cpp
+++ Graph Algorithms/Hamiltonian Flights.cpp
@@ -6,27 +6,32 @@ const int maxn=2000002;
 #define mod 1000000007
 #define pb  push_back
 #define pie 3.14159265358
-vector<int>v[21];
-ll dp[21][(1<<20)];
+vector < int > v[21];
+ll dp [21][(1<<20)];
 ll n;
 ll solve(ll fl,ll mask)
 {
 
-  ll ans=0;
-  if(mask==(1<<n)-1){
-   //ans++;
+  ll ans = 0;
+  
+  if(mask == (1<<n)- 1){
     return 1;
   }
-  if(fl==n)
+  
+  if(fl == n)
     return 0;
+  
   if(dp[fl][mask]!=-1)
     return dp[fl][mask];
  
- for(auto it:v[fl])
+ for(auto it : v[fl])
  {
-  if((mask&(1<<(it-1)))==0)
-  ans=(ans+solve(it,mask|(1<<(it-1))))%mod;
+   
+  if( ( mask&(1<<(it-1)) ) == 0)
+  ans=(ans+ solve( it , mask|(1<<(it-1)) ))%mod;
+   
  }
+  
   dp[fl][mask]=ans;
 
   return ans;
@@ -35,15 +40,21 @@ ll solve(ll fl,ll mask)
 int main()
 {
  
- int t;
- ll i,j,k,l=0,m,s1=0,pre=-1;
+
+  
+ ll i,j,k,l=0,m;
+  
  cin>>n>>m;
   for(i=1;i<=m;i++)
   {
     cin>>j>>k;
+    
     v[j].pb(k);
+
   }
   memset(dp,-1,sizeof(dp));
-  j=1;
+ 
+  
   cout<<solve(1,1);
+  
 }
